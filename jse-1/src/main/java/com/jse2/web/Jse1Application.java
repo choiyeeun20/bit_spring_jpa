63a843a7435485abf3fa954dfd3a6b952e49c1df
choiyunjung96 <dear__yoon@naver.com>
2020-04-28 12:45:43 +0900

    200428 12:45

---
+++ jse-1/src/main/java/com/jse2/web/Jse1Application.java
@@ -0,0 +1,13 @@
+package com.jse2.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jse1Application.class, args);
+	}
+
+}
---
+++ jse-1/src/test/java/com/jse2/web/Jse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jse2.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
